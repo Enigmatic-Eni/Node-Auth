4e38148cded9dfbcc931eb7433efea02d7ffe352
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-06-10 17:28:41 +0100

    First of all, introduction - register user works

---
+++ controller/auth-controller.js
@@ -0,0 +1,68 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
+
+// register controller
+const registerUser = async(req, res)=>{
+    try{
+        // extract user information fromthe req.body
+        const {username, password, email, role} = req.body;
+        
+        // check if user already exists in the database
+        const checkExistingUser = await User.findOne({$or : [{username}, {email}]});
+        if(checkExistingUser){
+            return res.status(400).json({
+                success: false,
+                message : "User with the same email or password already exists."
+            })
+        }
+
+        // hash user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // create a new user and save in the database
+        const newUser = new User({
+            username,
+            email,
+            password : hashedPassword,
+            role : role || "user"
+        })
+
+        await newUser.save()
+
+        if(newUser){
+            res.status(201).json({
+                success: true,
+                message : "User registered successfully"
+            })
+        }else{
+            res.status(400).json({
+                success: false,
+                message : "Unable to register user, please try again"
+            })
+        }
+    }catch(e){
+       console.log(e);
+       res.status(500).json({
+        success : false,
+        message : "Something went wrong, Please try again"
+       });
+       
+
+    }
+}
+
+// login controller 
+const loginUser = async(req, res)=>{
+    try{
+
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            success : false,
+            message : "Incorrect email or password"
+        });
+    }
+};
+
+module.exports = {registerUser, loginUser}
