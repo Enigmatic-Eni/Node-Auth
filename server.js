661c852e862ccf1431099b90fe83cf3a80f5f24c
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-08-23 23:28:52 +0100

    .

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const connectToDB = require('./database/db')
 const authRoutes = require('./routes/auth-routes')
 const homeRoute = require('./routes/home-route')
 const adminRoute = require('./routes/admin-route')
+const cors = require('cors')
 
 connectToDB()
 
 const app = express();
 
+
+
 const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json())
+
+ app.get('/text-cors', (req, res) => {
+  res.json({ message: 'CORS is working!' });
+});
 
 app.use('/auth', authRoutes);
 app.use('/home', homeRoute);
